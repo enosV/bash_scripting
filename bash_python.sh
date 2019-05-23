2bd1c958e7b239e115dee37fb4ded1cae0003e7b
enos <enosvazquez@gmail.com>
2019-05-22 19:55:16 -0700

    python from within bash

---
+++ bash_python.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+num1=1.3
+num2=3.4
+numx=$(python -c "print $num1 + $num2")
+echo $numx
--- bash_ruby.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-num1=1.3
-num2=3.4
-numx=$(ruby "print $num1 + $num2")
-echo numx
